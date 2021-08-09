60328debb5cede4ab6a93566aa7057fefd49afe2
zqlimy17 <zqlimy@gmail.com>
2021-08-09 13:40:38 +0800

    added link to logo

--- src/components/navbar/Logo.jsx
+++ src/components/navbar/Logo.jsx
@@ -1,31 +1,37 @@
 import React from "react";
+import { Link } from "gatsby";
 import styled from "styled-components";
 
 const Logo = () => {
   return (
     <Wrapper>
-      <p className="bold">
-        <span className="fade">Z</span>
-        <span className="fade" style={{ animationDelay: "50ms" }}>
-          Q
-        </span>
-        <span className="fade" style={{ animationDelay: "100ms" }}>
-          L
-        </span>
-        <span className="fade" style={{ animationDelay: "150ms" }}>
-          I
-        </span>
-        <span className="fade" style={{ animationDelay: "200ms" }}>
-          M
-        </span>
-        <span className="fade" style={{ animationDelay: "250ms" }}>
-          Y
-        </span>
-      </p>
+      <Link to="/">
+        <p className="bold">
+          <span className="fade">Z</span>
+          <span className="fade" style={{ animationDelay: "50ms" }}>
+            Q
+          </span>
+          <span className="fade" style={{ animationDelay: "100ms" }}>
+            L
+          </span>
+          <span className="fade" style={{ animationDelay: "150ms" }}>
+            I
+          </span>
+          <span className="fade" style={{ animationDelay: "200ms" }}>
+            M
+          </span>
+          <span className="fade" style={{ animationDelay: "250ms" }}>
+            Y
+          </span>
+        </p>
+      </Link>
     </Wrapper>
   );
 };
 const Wrapper = styled.div`
+  a {
+    text-decoration: none;
+  }
   p {
     margin: 0;
     font-size: 1.5rem;
